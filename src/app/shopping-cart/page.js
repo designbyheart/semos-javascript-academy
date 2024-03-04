91891da135efcd988ab19faf8b29cb6f66b2744f
Pedja Jevtic <designbyheart@gmail.com>
2024-03-04 20:47:43 +0100

    Loading products when page loads

--- src/app/shopping-cart/page.js
+++ src/app/shopping-cart/page.js
@@ -1,7 +1,19 @@
 'use client';
+import { useEffect, useState } from 'react'
+
 import './shopping-cart.css';
+import { apiLoadProducts } from "../../services/api/apiProducts";
 
 export default function ShoppingCartPage() {
+    const [products, setProducts] = useState([]);
+
+    const loadProduct = async () => {
+        console.log("products", await apiLoadProducts());
+    }
+
+    useEffect(() => {
+        loadProduct();
+    }, []);
 
 return <div className="shopping-cart-container">
     <h1>Shopping Cart</h1>
