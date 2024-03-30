fb776fc452b9a42126265d6d28c89641363c6903
Pedja Jevtic <designbyheart@gmail.com>
2024-03-30 12:23:13 +0100

    Dodat api za hotele, apartmane i pretragu

--- src/app/api/search/route.js
+++ src/app/api/search/route.js
@@ -1,11 +1,27 @@
-import { hotels } from '@/data/mocked/hotels';
 import { apartments } from '@/data/mocked/apartments';
+import { hotels } from '@/data/mocked/hotels';
 import { NextResponse } from 'next/server';
 
-
 export const GET = async (request, { params }) => {
-    const {searchParams} = new URL(request.url);
-    const name = searchParams.get("name");
-    let result = [...hotels, ...apartments].filter((item) => item.name.toLowerCase().includes(name.toLowerCase()));
+    const { searchParams } = new URL(request.url);
+    const name             = searchParams.get('name');
+    const isAvailable      = searchParams.get('available') || null;
+    const type             = searchParams.get('type');
+
+    let result = [...hotels, ...apartments].filter((item) => {
+        let shouldReturn = item
+            .name.toLowerCase()
+            .includes(name.toLowerCase());
+
+        if (isAvailable !== null) {
+            shouldReturn = shouldReturn && `${item.isAvailable}` === isAvailable;
+        }
+
+        if(type) {
+            shouldReturn = shouldReturn && item.type === type;
+        }
+
+        return shouldReturn;
+    });
     return NextResponse.json(result);
 };
