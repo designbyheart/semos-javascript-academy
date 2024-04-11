d54c7138a697d1a4b445eb1188326b1c029ee875
Shomy <26857315+ShomyTKD@users.noreply.github.com>
2024-04-11 17:58:10 +0200

    Destination List style updated

--- src/components/organisms/DestinationList.js
+++ src/components/organisms/DestinationList.js
@@ -6,25 +6,27 @@ import TabBar from '@/components/molecules/TabBar';
 export default function DestinationList({ destinations, activeTab, onTabChange, searchValue, onSearchChange }) {
     return (
         <div className="destination-list">
-            <h2>Find and take the trip you want</h2>
-            <div>
-                <TabBar tabs={['All', 'Best seller', 'Nature', 'Seasonal', 'City']}
+            <div className="destination-wrapper">
+                <h2>Find and take the trip you want</h2>
+                <div className='destination-navtabs'>
+                    <TabBar tabs={['All', 'Best seller', 'Nature', 'Seasonal', 'City']}
                         activeTab={activeTab}
                         onTabClick={onTabChange}
-                />
-                <SearchField value={searchValue} onChange={onSearchChange}/>
-            </div>
-            <div>
-                {destinations?.map((destination) => {
-                    return (
-                        <div key={destination.id}>
-                          <a href={destination.link}>
-                            <img src={destination.image} alt={destination.name}/>
-                            <h3>{destination.name}</h3>
-                          </a>
-                        </div>
-                    );
-                })}
+                    />
+                    {/* <SearchField value={searchValue} onChange={onSearchChange} /> */}
+                </div>
+                <div className="destination-grid">
+                    {destinations?.map((destination) => {
+                        return (
+                            <div className="destination-item" key={destination.id}>
+                                <a href={destination.link}>
+                                    <img className="destination-image" src={destination.image} alt={destination.name} />
+                                    <h3 className="destination-name">{destination.name}</h3>
+                                </a>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
         </div>
     );
